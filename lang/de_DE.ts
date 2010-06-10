f3bda828b0efacd66c6ea2a71e2737abbc404539
jonas <jonas@JonesNetBook.jolicloud-domain>
2010-06-10 20:08:27 +0200

    Translations
     + german language file and compilation

---
+++ lang/de_DE.ts
@@ -0,0 +1,507 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<context>
+    <name>Competition</name>
+    <message>
+        <location filename="../data/competition.cpp" line="34"/>
+        <source>Open error!</source>
+        <translation>Fehler beim Öffnen!</translation>
+    </message>
+    <message>
+        <location filename="../data/competition.cpp" line="34"/>
+        <source>Competition could not be opened due to the following error: </source>
+        <translation>Der Wettkampf konnte nicht geöffnet werden. Fehler: </translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>OpenCompetition</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="35"/>
+        <source>Registration</source>
+        <translation>Meldungen</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="47"/>
+        <source>Import..</source>
+        <translation>Importieren..</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="57"/>
+        <source>Registered clubs</source>
+        <translation>Gemeldete Vereine</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="67"/>
+        <source>Competitors</source>
+        <translation>Wettkämpfer</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="77"/>
+        <source>Judges</source>
+        <translation>Kampfrichter</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="90"/>
+        <source>Competition</source>
+        <translation>Wettkampf</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="102"/>
+        <source>Starting list</source>
+        <translation>Startliste</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="112"/>
+        <source>Score input</source>
+        <translation>Wertungseingabe</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="122"/>
+        <source>Beamer output</source>
+        <translation>Beamer Ausgabe</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="138"/>
+        <source>Results</source>
+        <translation>Ergebnisse</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="153"/>
+        <source>Certificates</source>
+        <translation>Urkunden</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="163"/>
+        <source>Result list</source>
+        <translation>Ergebnisliste</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="173"/>
+        <source>Ranking points</source>
+        <translation>Ranglistenpunkte</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="201"/>
+        <source>File</source>
+        <translation>Datei</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="214"/>
+        <source>Close</source>
+        <translation>Beenden</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="219"/>
+        <source>Open..</source>
+        <translation>Öffnen..</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="224"/>
+        <source>Save</source>
+        <translation>Speichern</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="229"/>
+        <source>Save as..</source>
+        <translation>Speichern unter..</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.cpp" line="106"/>
+        <source>Wettkampf speichern</source>
+        <translation>Wettkampf speichern</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.cpp" line="108"/>
+        <source>Wettkampf (*.cmp);; AllFiles (*)</source>
+        <translation>Wettkampf (*.cmp;; Alle Dateien (*)</translation>
+    </message>
+</context>
+<context>
+    <name>NewCompDialog</name>
+    <message>
+        <location filename="../ui/newcompdialog.ui" line="14"/>
+        <source>Create new competition</source>
+        <translation>Neuen Wettkampf erstellen</translation>
+    </message>
+    <message>
+        <location filename="../ui/newcompdialog.ui" line="22"/>
+        <source>Name:</source>
+        <translation>Name:</translation>
+    </message>
+    <message>
+        <location filename="../ui/newcompdialog.ui" line="39"/>
+        <source>Date:</source>
+        <translation>Datum:</translation>
+    </message>
+    <message>
+        <location filename="../ui/newcompdialog.ui" line="59"/>
+        <source>dd.MM.yyyy</source>
+        <translation>dd.MM.yyyy</translation>
+    </message>
+    <message>
+        <location filename="../ui/newcompdialog.ui" line="69"/>
+        <source>Time:</source>
+        <translation>Zeit:</translation>
+    </message>
+    <message>
+        <location filename="../ui/newcompdialog.ui" line="82"/>
+        <source>HH:mm</source>
+        <translation>HH:mm</translation>
+    </message>
+    <message>
+        <location filename="../ui/newcompdialog.ui" line="98"/>
+        <source>This competition counts for the national ranking</source>
+        <translation>Dieser Wettkampf ist ein Ranglistenturnier</translation>
+    </message>
+    <message>
+        <location filename="../ui/newcompdialog.ui" line="107"/>
+        <source>Description:</source>
+        <translation>Beschreibung:</translation>
+    </message>
+</context>
+<context>
+    <name>OpenDialog</name>
+    <message>
+        <location filename="../ui/opendialog.ui" line="23"/>
+        <source>Open competition</source>
+        <translation>Wettkampf öffnen</translation>
+    </message>
+    <message>
+        <location filename="../ui/opendialog.ui" line="43"/>
+        <source>File</source>
+        <translation>Datei</translation>
+    </message>
+    <message>
+        <location filename="../ui/opendialog.ui" line="52"/>
+        <source>Create new file...</source>
+        <translation>Neue Datei erstellen...</translation>
+    </message>
+    <message>
+        <location filename="../ui/opendialog.ui" line="83"/>
+        <source>Open new file dialog...</source>
+        <translation>Dialog für neue Datei öffnen...</translation>
+    </message>
+    <message>
+        <location filename="../ui/opendialog.ui" line="92"/>
+        <source>Load recent file...</source>
+        <translation>Kürzlich verwendet...</translation>
+    </message>
+    <message>
+        <location filename="../ui/opendialog.ui" line="133"/>
+        <source>History</source>
+        <translation>Verlauf</translation>
+    </message>
+    <message>
+        <location filename="../ui/opendialog.ui" line="149"/>
+        <source>Open existing file...</source>
+        <translation>Existierende Datei öffnen...</translation>
+    </message>
+    <message>
+        <location filename="../ui/opendialog.ui" line="177"/>
+        <source>Open..</source>
+        <translation>Öffnen..</translation>
+    </message>
+    <message>
+        <location filename="../ui/opendialog.ui" line="195"/>
+        <source>Description</source>
+        <translation>Beschreibung</translation>
+    </message>
+    <message>
+        <location filename="../ui/opendialog.ui" line="218"/>
+        <source>Name:</source>
+        <translation>Name:</translation>
+    </message>
+    <message>
+        <location filename="../ui/opendialog.ui" line="248"/>
+        <source>Date:</source>
+        <translation>Datum:</translation>
+    </message>
+    <message>
+        <location filename="../ui/opendialog.ui" line="278"/>
+        <source>Time:</source>
+        <translation>Zeit:</translation>
+    </message>
+    <message>
+        <location filename="../ui/opendialog.ui" line="303"/>
+        <source>Ranking competition</source>
+        <translation>Ranglistenturnier</translation>
+    </message>
+    <message>
+        <location filename="../ui/opendialog.ui" line="324"/>
+        <source>Detailed description:</source>
+        <translation>Beschreibung:</translation>
+    </message>
+    <message>
+        <location filename="../ui/opendialog.cpp" line="193"/>
+        <source>Competition *.cmp</source>
+        <translation>Wettkampf (*.cmp)</translation>
+    </message>
+    <message>
+        <location filename="../ui/opendialog.cpp" line="207"/>
+        <source>Specify the name of the competition.</source>
+        <translation>Name des Wettkampfes angeben.</translation>
+    </message>
+    <message>
+        <location filename="../ui/opendialog.cpp" line="210"/>
+        <source>Properties - </source>
+        <translation>Eigenschaften - </translation>
+    </message>
+    <message>
+        <location filename="../ui/opendialog.cpp" line="223"/>
+        <source>Open Competition.</source>
+        <translation>Wettkampf öffnen.</translation>
+    </message>
+    <message>
+        <location filename="../ui/opendialog.cpp" line="236"/>
+        <source>Open Error.</source>
+        <translation>Fehler beim Öffnen.</translation>
+    </message>
+    <message>
+        <location filename="../ui/opendialog.cpp" line="237"/>
+        <source>Opening competition file failed.</source>
+        <translation>Öffnen der Wettkampfdatei fehlgeschlagen.</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../data/group.cpp" line="8"/>
+        <source>IW</source>
+        <translation>Einzel Weiblich</translation>
+    </message>
+    <message>
+        <location filename="../data/group.cpp" line="9"/>
+        <source>Mixed</source>
+        <translation>Gemischtes Paar</translation>
+    </message>
+    <message>
+        <location filename="../data/group.cpp" line="10"/>
+        <source>Pair/Trio</source>
+        <translation>2er-3er Team</translation>
+    </message>
+    <message>
+        <location filename="../data/group.cpp" line="11"/>
+        <source>Group</source>
+        <translation>Gruppe</translation>
+    </message>
+    <message>
+        <location filename="../data/judge.cpp" line="7"/>
+        <source>Artistic</source>
+        <translation>Artistik</translation>
+    </message>
+    <message>
+        <location filename="../data/judge.cpp" line="8"/>
+        <source>Execution</source>
+        <translation>Ausführung</translation>
+    </message>
+    <message>
+        <location filename="../data/judge.cpp" line="9"/>
+        <source>Difficulty</source>
+        <translation>Schwierigkeit</translation>
+    </message>
+    <message>
+        <location filename="../data/judge.cpp" line="10"/>
+        <source>Chair</source>
+        <translation>Oberkampfrichter</translation>
+    </message>
+    <message>
+        <location filename="../data/judge.cpp" line="11"/>
+        <source>Superior</source>
+        <translation>Superior Jury</translation>
+    </message>
+    <message>
+        <location filename="../data/judge.cpp" line="12"/>
+        <source>Assistant</source>
+        <translation>OK Assistent</translation>
+    </message>
+    <message>
+        <location filename="../data/judge.cpp" line="20"/>
+        <source>LTV</source>
+        <translation>LTV</translation>
+    </message>
+    <message>
+        <location filename="../data/judge.cpp" line="21"/>
+        <source>DTB</source>
+        <translation>DTB</translation>
+    </message>
+    <message>
+        <location filename="../data/judge.cpp" line="22"/>
+        <source>FIG4</source>
+        <translation>FIG4</translation>
+    </message>
+    <message>
+        <location filename="../data/judge.cpp" line="23"/>
+        <source>FIG3</source>
+        <translation>FIG3</translation>
+    </message>
+    <message>
+        <location filename="../data/judge.cpp" line="24"/>
+        <source>FIG2</source>
+        <translation>FIG2</translation>
+    </message>
+    <message>
+        <location filename="../data/judge.cpp" line="25"/>
+        <source>FIG1</source>
+        <translation>FIG1</translation>
+    </message>
+    <message>
+        <location filename="../data/judge.cpp" line="26"/>
+        <source>NONE</source>
+        <translation>Kein Brevet</translation>
+    </message>
+    <message>
+        <location filename="../data/tomanycompetitorsexception.cpp" line="11"/>
+        <source>Group size limit reached. Add a substitute or change group type.</source>
+        <translation>Maximale Gruppengröße erreicht. Gruppentyp ändern oder Ersatzstarter hinzufügen.</translation>
+    </message>
+    <message>
+        <location filename="../modules/import/filenotopenedexception.cpp" line="12"/>
+        <source>Opening file failed. It may not exist or is in use by another program.</source>
+        <translation>Öffnen der Datei fehlgeschlagen. Die datei ist möglicherweise nicht vorhanden oder wird von einem anderen Programm benutzt.</translation>
+    </message>
+</context>
+<context>
+    <name>QObject::QObject</name>
+    <message>
+        <location filename="../data/group.cpp" line="7"/>
+        <source>IM</source>
+        <translation>Einzel Männlich</translation>
+    </message>
+</context>
+<context>
+    <name>importModule</name>
+    <message>
+        <location filename="../modules/import/importmodule.ui" line="14"/>
+        <source>Importieren...</source>
+        <translation>Importieren...</translation>
+    </message>
+    <message utf8="true">
+        <source>1. Meldungen auswählen</source>
+        <translation type="obsolete">1. Melsung auswählen</translation>
+    </message>
+    <message utf8="true">
+        <source>Hinzufügen</source>
+        <translation type="obsolete">Hinzufügen</translation>
+    </message>
+    <message>
+        <source>Entfernen</source>
+        <translation type="obsolete">Entfernen</translation>
+    </message>
+    <message>
+        <source>Leeren</source>
+        <translation type="obsolete">Leeren</translation>
+    </message>
+    <message>
+        <source>Abbrechen</source>
+        <translation type="obsolete">Abbrechen</translation>
+    </message>
+    <message>
+        <source>Weiter</source>
+        <translation type="obsolete">Weiter</translation>
+    </message>
+    <message>
+        <source>2. Importieren</source>
+        <translation type="obsolete">2. Importieren</translation>
+    </message>
+    <message utf8="true">
+        <source>3. Abschließen</source>
+        <translation type="obsolete">3. Abschließen</translation>
+    </message>
+    <message>
+        <source>Folgende Vereine wurden importiert:</source>
+        <translation type="obsolete">Folgende Vereine wurden importiert:</translation>
+    </message>
+    <message>
+        <location filename="../modules/import/importmodule.ui" line="36"/>
+        <source>1. Select Registrations</source>
+        <translation>1. Meldung auswählen</translation>
+    </message>
+    <message>
+        <location filename="../modules/import/importmodule.ui" line="79"/>
+        <source>Add</source>
+        <translation>Hinzufügen</translation>
+    </message>
+    <message>
+        <location filename="../modules/import/importmodule.ui" line="86"/>
+        <source>Remove</source>
+        <translation>Entfernen</translation>
+    </message>
+    <message>
+        <location filename="../modules/import/importmodule.ui" line="93"/>
+        <source>Clear</source>
+        <translation>Leeren</translation>
+    </message>
+    <message>
+        <location filename="../modules/import/importmodule.ui" line="120"/>
+        <location filename="../modules/import/importmodule.ui" line="187"/>
+        <location filename="../modules/import/importmodule.ui" line="310"/>
+        <source>Cancel</source>
+        <translation>Abbrechen</translation>
+    </message>
+    <message>
+        <location filename="../modules/import/importmodule.ui" line="127"/>
+        <location filename="../modules/import/importmodule.ui" line="194"/>
+        <source>Continue</source>
+        <translation>Weiter</translation>
+    </message>
+    <message>
+        <location filename="../modules/import/importmodule.ui" line="146"/>
+        <source>2. Import</source>
+        <translation>2. Importieren</translation>
+    </message>
+    <message>
+        <location filename="../modules/import/importmodule.ui" line="213"/>
+        <source>3. Finish</source>
+        <translation>3. Fertig</translation>
+    </message>
+    <message>
+        <location filename="../modules/import/importmodule.ui" line="225"/>
+        <source>The following clubs have been importet:</source>
+        <translation>Die folgenen Vereine wurden importiert:</translation>
+    </message>
+    <message>
+        <location filename="../modules/import/importmodule.ui" line="245"/>
+        <source>Verein</source>
+        <translation>Vereine</translation>
+    </message>
+    <message>
+        <location filename="../modules/import/importmodule.ui" line="256"/>
+        <source>Newcomer</source>
+        <translation>Newcomer</translation>
+    </message>
+    <message>
+        <location filename="../modules/import/importmodule.ui" line="267"/>
+        <source>Cup</source>
+        <translation>Cup</translation>
+    </message>
+    <message>
+        <location filename="../modules/import/importmodule.ui" line="278"/>
+        <source>Masters</source>
+        <translation>Masters</translation>
+    </message>
+    <message>
+        <location filename="../modules/import/importmodule.ui" line="317"/>
+        <source>Ready</source>
+        <translation>Fertig</translation>
+    </message>
+    <message>
+        <source>Fertig</source>
+        <translation type="obsolete">Fertig</translation>
+    </message>
+    <message>
+        <source>Meldungen auswählen</source>
+        <translation type="obsolete">Meldung auswählen</translation>
+    </message>
+    <message>
+        <location filename="../modules/import/importmodule.cpp" line="46"/>
+        <source>Select Registration</source>
+        <translation>Meldung auswählen</translation>
+    </message>
+</context>
+</TS>
